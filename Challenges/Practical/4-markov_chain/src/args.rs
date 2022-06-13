2ffc95099446c2ee71271fa28d455471e8edb336
elmaxondscrd <maxdc7613@gmail.com>
2022-06-13 09:51:40 +0000

    Put args in its own module

---
+++ Challenges/Practical/4-markov_chain/src/args.rs
@@ -0,0 +1,57 @@
+use std::{path::PathBuf, process::exit};
+
+pub struct Parameters {
+  pub input_file: PathBuf,
+  pub sentence_size: usize,
+}
+
+const HELP_MESSAGE: &str = "Markov Chain Sentence Generator - A quick tool used to generate sentences based on an input using a discreet Markov chain algorithm.
+ 
+Usage: markov_chain_sentence_generator (flags) [path] [size]
+
+Flags/Parameters :
+  --help (-h)        Prints this help message!
+  path               Specify the path of the file containaing the input text that will be fed to the program to generate a sentence from.
+  size               Specify the number of words in the resulting sentence. Must be a valid, strictly-positive number.";
+
+pub fn get_args() -> Parameters {
+  // Stores the arguments provided by the user in a Vector of Strings
+  let args = std::env::args().collect::<Vec<String>>();
+
+  if args.contains(&String::from("--help"))
+    || args.contains(&String::from("-h"))
+  {
+    println!("{HELP_MESSAGE}");
+    exit(0)
+  }
+
+  // If no arguments are provided or the arguments provided are wrong, prints
+  // the help or the program.
+  // (exit code 22 means "Invalid argument")
+  if args.len() < 2 || args.len() > 3 {
+    println!("{HELP_MESSAGE}");
+    exit(22)
+  }
+
+  // Initializes the `path` parameter, and exits if the file is inexistant.
+  // (exit code 2 means "No such file or directory")
+  let path = PathBuf::from(&args[1]);
+  if !path.exists() || !path.is_file() {
+    eprintln!("File `{}` was not found!", path.display());
+    exit(2)
+  }
+
+  // Initializes the `size` parameter, and exits if the value is unvalid.
+  // (exit code 22 means "Invalid argument")
+  let size = &args[2].parse::<usize>().unwrap_or_default();
+  if size.eq(&0) {
+    eprintln!("The size provided is not a valid, strictly positive number!");
+    exit(22)
+  }
+
+  // Returns the parameters!
+  Parameters {
+    input_file: path,
+    sentence_size: size.to_owned(),
+  }
+}
--- Challenges/Practical/4-markov_chain/src/main.rs
+++ Challenges/Practical/4-markov_chain/src/main.rs
@@ -6,29 +6,15 @@ use rand::{
 use regex::Regex;
 use std::{
   collections::HashMap,
-  env::args,
-  fs::read_to_string,
+  fs,
   io::{Error, ErrorKind},
-  path::PathBuf,
   process::exit,
 };
 
+mod args;
+
 type Transitions = HashMap<String, HashMap<String, usize>>;
 type States = Vec<String>;
-
-const HELP_MESSAGE: &str = "Markov Chain Sentence Generator - A quick tool used to generate sentences based on an input using a discreet Markov chain algorithm.
- 
-Usage: markov_chain_sentence_generator (flags) [path] [size]
-
-Flags/Parameters :
-  --help (-h)        Prints this help message!
-  path               Specify the path of the file containaing the input text that will be fed to the program to generate a sentence from.
-  size               Specify the number of words in the resulting sentence. Must be a valid, strictly-positive number.";
-
-struct Parameters {
-  input_file: PathBuf,
-  sentence_size: usize,
-}
 
 struct SentenceMarkovChain {
   states: States,
@@ -139,53 +125,11 @@ impl SentenceMarkovChain {
   }
 }
 
-fn get_args() -> Parameters {
-  // Stores the arguments provided by the user in a Vector of Strings
-  let args = args().collect::<Vec<String>>();
-
-  if args.contains(&String::from("--help"))
-    || args.contains(&String::from("-h"))
-  {
-    println!("{HELP_MESSAGE}");
-    exit(0)
-  }
-
-  // If no arguments are provided or the arguments provided are wrong, prints
-  // the help or the program.
-  // (exit code 22 means "Invalid argument")
-  if args.len() < 2 || args.len() > 3 {
-    println!("{HELP_MESSAGE}");
-    exit(22)
-  }
-
-  // Initializes the `path` parameter, and exits if the file is inexistant.
-  // (exit code 2 means "No such file or directory")
-  let path = PathBuf::from(&args[1]);
-  if !path.exists() || !path.is_file() {
-    eprintln!("File `{}` was not found!", path.display());
-    exit(2)
-  }
-
-  // Initializes the `size` parameter, and exits if the value is unvalid.
-  // (exit code 22 means "Invalid argument")
-  let size = &args[2].parse::<usize>().unwrap_or_default();
-  if size.eq(&0) {
-    eprintln!("The size provided is not a valid, strictly positive number!");
-    exit(22)
-  }
-
-  // Returns the parameters!
-  Parameters {
-    input_file: path,
-    sentence_size: size.to_owned(),
-  }
-}
-
 fn main() {
   // Gets the user-provided parameters.
-  let parameters = get_args();
+  let parameters = args::get_args();
 
-  let input_text = match read_to_string(parameters.input_file) {
+  let input_text = match fs::read_to_string(parameters.input_file) {
     Ok(content) => content,
     Err(e) => {
       eprintln!("There was a problem trying to read the file: {e}");
